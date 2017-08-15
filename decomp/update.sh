24829e38d6c17fae8dbcf6a664e2ba313c766ebc
mewmew <rnd0x00@gmail.com>
2017-08-15 23:24:59 +0200

    decomp: remove vanity imports

--- decomp/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for f in `go list github.com/decomp/decomp/... | sar "github.com/decomp/decomp/" ""`; do
-	echo ${f}
-	mkdir -p ${f}
-	cp index.html ${f}/index.html
-	sar -i "godoc.org/decomp.org/decomp" "godoc.org/decomp.org/decomp/${f}" ${f}/index.html
-done
